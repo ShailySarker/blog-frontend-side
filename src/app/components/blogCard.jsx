c2fbd41f1da70db7fbd3f2155bd234f76629ca71
ShailySarker <shailysarker99@gmail.com>
2024-09-15 02:28:56 +0600

    all post show and delete single post work completed

--- src/app/components/blogCard.jsx
+++ src/app/components/blogCard.jsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 
 export default function BlogCard({ post, onDelete }) {
   return (
-    <div className="p-4 border rounded-lg shadow-lg">
-      <h3 className="text-xl font-bold">{post?.title}</h3>
-      <p className="text-gray-700">{post?.content.slice(0, 100)}...</p>
-      <div className="mt-4">
+    <div className="lg:p-6 p-4 shadow-lg bg-white border-2 border-slate-400 rounded-xl">
+      <h3 className="md:text-xl font-bold">{post?.title}</h3>
+      <p className="text-gray-700 lg:mt-3 md:mt-2 mt-1">{post?.content.slice(0, 100)}...</p>
+      <div className="lg:mt-8 md:mt-7 mt-6 flex justify-center">
         <Link href={`/post/${post?.id}`}>
-          <Button className="mr-2">View</Button>
+          <Button className="mr-2 w-28 font-semibold md:text-lg">View</Button>
         </Link>
-        <Button variant="destructive" onClick={onDelete}>
+        <Button className="w-28 font-semibold md:text-lg " variant="destructive" onClick={onDelete}>
           Delete
         </Button>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import BlogCard from './components/blogCard';
+import api from '@/lib/api';
+import ConfirmModal from './components/confirmModal';
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -10,7 +12,7 @@ export default function Home() {
 
   useEffect(() => {
     async function fetchPosts() {
-      const response = await axios.get('http://localhost:4000/posts');
+      const response = await api.get('/posts');
       setPosts(response?.data);
     }
     fetchPosts();
@@ -18,23 +20,30 @@ export default function Home() {
   console.log(posts)
 
   const deletePost = async (id) => {
-    await axios.delete(`/posts/${id}`);
+    await api.delete(`/posts/${id}`);
     setPosts(posts.filter(post => post?.id !== id));
   };
 
   return (
-    <div>
-      <div className="p-6">
-        <h2 className="text-2xl font-bold mb-4">All Blog Posts</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {posts?.map(post => (
-            <BlogCard key={post?.id} post={post} onDelete={() => {
-              setDeletePostId(post?.id);
-              setShowModal(true);
-            }} />
-          ))}
-        </div>
+    <div className="lg:pt-6 md:pt-4 pt-3 lg:pb-28 md:pb-24 pb-14">
+      <h2 className="lg:text-3xl md:text-xl text-lg text-center font-bold lg:mb-4 md:mb-3 mb-2">All Blog Posts</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 md:gap-5 gap-4 lg:mt-12 md:mt-9 mt-7">
+        {posts?.map(post => (
+          <BlogCard key={post?.id} post={post} onDelete={() => {
+            setDeletePostId(post?.id);
+            setShowModal(true);
+          }} />
+        ))}
       </div>
+      {showModal && (
+        <ConfirmModal
+          onConfirm={() => {
+            deletePost(deletePostId);
+            setShowModal(false);
+          }}
+          onCancel={() => setShowModal(false)}
+        />
+      )}
     </div>
   );
 }
