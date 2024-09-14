d87ba1ba88f993067d87c640df51aae5a6811082
ShailySarker <shailysarker99@gmail.com>
2024-09-14 15:11:05 +0600

    shadCN setup completed

---
+++ src/app/createPost/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const CreateBlog = () => {
+    return (
+        <div>
+            <h2>Create Post</h2>
+            <Button>Create Post</Button>
+        </div>
+    );
+};
+
+export default CreateBlog;
