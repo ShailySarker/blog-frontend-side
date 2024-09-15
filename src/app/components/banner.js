081b2eb24b5e81c2846ad5976a873e7ca18cf3f4
ShailySarker <shailysarker99@gmail.com>
2024-09-15 20:30:38 +0600

    banner section updated

--- src/app/components/banner.js
+++ src/app/components/banner.js
@@ -11,7 +11,7 @@ export default function Banner() {
                 <Image src={banner} alt="banner image" />
             </div>
             <div className="lg:w-[50%] w-[100%]">
-                <h2 className="lg:text-4xl md:text-3xl text-2xl font-semibold">Welcome to <span className="font-bold text-[#FD6F00]">Blogs Corner</span></h2>
+                <h2 className="lg:text-4xl md:text-3xl text-2xl font-semibold text-black">Welcome to <span className="font-bold text-[#FD6F00]">Blogs Corner</span></h2>
                 <p className="text-gray-700 lg:mt-5 md:mt-4 mt-3 text-justify lg:text-lg/loose md:text-base/relaxed text-base">Blogs Corner is a dynamic platform where users can explore a variety of topics, share their thoughts, and engage with a diverse community. Whether you are looking for insights on technology, lifestyle, personal growth, or creative writing, Blogs Corner offers a space for both readers and writers to connect.</p>
                 <Link href="/createPost">
                     <Button className="lg:mt-8 md:mt-6 mt-4 w-36 font-semibold md:text-lg">Get Start</Button>
